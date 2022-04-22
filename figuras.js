a4d1378df9cd9df9f4efba8c090274c34bef35ea
unknown <r.marin22@hotmail.com>
2022-04-22 08:03:12 -0500

    estamos de fincho subiendo cambios para estudiar en casa

---
+++ figuras.js
@@ -0,0 +1,24 @@
+//codigo del cuadrado
+console.group("cuadrado");
+const ladoCuadrado = 5;
+console.log("los lados de mi cuadrado miden:" + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es:" + perimetroCuadrado);
+
+s
+console.log("El perimetro del cuadrado es:" + areaCuadrada);
+console.groupEnd;
+//codigo del triangulo
+console.group("triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+
+console.log("Los lados del triangulo miden:" + ladoTriangulo1 + "cm,"+ ladoTriangulo2 + "cm,"+ baseTriangulo + "cm,");
+const alturaTriangulo = 5.5;
+console.log("la altura del trianguo es:" + alturaTriangulo);
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo ;
+console.log("El perimetro del traingulo es:" + perimetroTriangulo);
+console.groupEnd;
